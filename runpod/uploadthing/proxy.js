90fb92dcf4288845ccdcc78fdf5eaa63dfe66056
camenduru <54370274+camenduru@users.noreply.github.com>
2024-10-09 02:31:22 +0300

    cors

--- runpod/uploadthing/proxy.js
+++ runpod/uploadthing/proxy.js
@@ -1,29 +1,40 @@
 export default {
-    async fetch(request, env) {
-      if (request.method === 'POST') {
-        const requestBody = await request.json();
-        const url = `https://api.runpod.ai/v2/${env.RUNPOD_ID}/runsync`;
-        const headers = {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${env.RUNPOD_KEY}`,
-        };
-        const inResponse = await fetch(url, {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(requestBody),
-        });
-        if (!inResponse.ok) {
-          return new Response('500', { status: 500 });
+  async fetch(request, env) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         }
-        const outResponse = await inResponse.json();
-        return new Response(JSON.stringify(outResponse), {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-      } else {
-        return new Response('400', { status: 400 });
+      });
+    }
+    if (request.method === 'POST') {
+      const requestBody = await request.json();
+      const url = `https://api.runpod.ai/v2/${env.RUNPOD_ID}/runsync`;
+      const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${env.RUNPOD_KEY}`,
+      };
+      const inResponse = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(requestBody),
+      });
+      if (!inResponse.ok) {
+        return new Response('500', { status: 500 });
       }
-    },
-  };
-  
+      const outResponse = await inResponse.json();
+      return new Response(JSON.stringify(outResponse), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+      });
+    } else {
+      return new Response('400', { status: 400 });
+    }
+  },
+};
